a3b72ab92ba56396d321ce70fd161344524146f5
VladEgrv <vladegrv@yandex.ru>
2023-12-04 20:48:34 +0300

    adventofcode_d2p2_solution

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-target = '7'
-list_of_numbers = {
-      '1' : ['1', 'one'],
-      '2' : ['2', 'two'],
-      '3' : ['3', 'three'], 
-      '4' : ['4', 'four'], 
-      '5' : ['5', 'five'], 
-      '6' : ['6', 'six'], 
-      '7' : ['7', 'seven'], 
-      '8' : ['8', 'eight'], 
-      '9' : ['9', 'nine']}
-for key, value in list_of_numbers.items():
-        if target in value:
-            print(key)
+rounds_of_game = [{'red': 9, 'green': 12, 'blue': 2}, {'blue': 1, 'green': 11, 'red': 32}, {'red': 10, 'green': 87}]
+
+minimum_set = {'red' : 1, 'green' : 1, 'blue' : 1}
+
+dict1 = {'blue': 1, 'green': 11, 'red': 10}
+dict2 = {'red': 1, 'green': 1, 'blue': 1}
+
+
+for run_of_game in rounds_of_game:
+    for colour, value in run_of_game.items():
+        if colour in minimum_set and minimum_set[colour] < value:
+            minimum_set[colour] = value
+
+print(minimum_set)
